abdf68061022757be2924d95be7849e353b9f8a1
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-14 22:40:06 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "៚ OWNER:[LEO ANGKASA](https://t.me/leoangkasaaa)\n \n៚ REPO:[SPACE-BOT](https://github.com/AngkasaBoy/Space-Bot) \n \n៚ Channel:[SUPPORT](https://t.me/storyangkasa)\n     \n"
+        "✦✧ ᴏᴡɴᴇʀ ✧✦:[LEO ANGKASA](https://t.me/leoangkasaaa)\n \n✦✧ ʀᴇᴘᴏsɪᴛᴏʀʏ ✧✦:[SPACE-BOT](https://github.com/AngkasaBoy/Space-Bot) \n \n✦✧ ᴄʜᴀɴɴᴇʟ ✧✦:[SUPPORT](https://t.me/storyangkasa)\n     \n"
     )
 
 
