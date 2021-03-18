33bb55f340a43fd7fcf4d8e4003e5ed613d63f2b
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-19 03:25:30 +0800

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -263,7 +263,7 @@ async def afk_on_pm(sender):
 
 
 CMD_HELP.update({
-    "afk bentar":
+    "mati":
     "`.afk` [Optional Reason]\
 \nUsage: Sets you as afk.\nReplies to anyone who tags/PM's \
 you telling them that you are AFK(reason).\n\nSwitches off AFK when you type back anything, anywhere.\
