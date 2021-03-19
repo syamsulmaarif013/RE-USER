737ed951e142e10cc2cede05e0b0643855e5f446
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-19 12:40:20 +0800

    Update animasi.py

--- userbot/modules/animasi.py
+++ userbot/modules/animasi.py
@@ -119,7 +119,7 @@ async def typewriter(typew):
     )
 
 
-@register(outgoing=True, pattern="^.run(?: |$)(.*)")
+@register(outgoing=True, pattern="^.kabur(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit(
@@ -326,7 +326,7 @@ CMD_HELP.update(
     \nUsage: liat sendiri\
     \n\n`.y`\
     \nUsage: jempol\
-    \n\n`.awkwok`\
+    \n\n`.kabur`\
     \nUsage: ketawa lari.\
     \n\n`.ular` ; `.babi` ; `.ajg`\
     \nUsage: liat sendiri."
