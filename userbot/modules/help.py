f11a672cf1784fb582ea509439021991c346ef40
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-26 22:53:07 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,7 +35,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\tΦ"
+            string += "`\t•"
         await event.reply(f"•{string}•"
                           "\nJANGAN TYPO ASUUU....")
         await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Module**")
