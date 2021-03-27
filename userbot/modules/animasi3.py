c87910a25dc6fc8f26078138a32be2eac5937c03
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-27 19:26:05 +0800

    Create animasi3.py

---
+++ userbot/modules/animasi3.py
@@ -0,0 +1,343 @@
+import asyncio
+from time import sleep
+
+from telethon import events
+
+from userbot import CMD_HELP, bot
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.sayang$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("AKU CINTA KAMU 💕")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💘💞💗💕")
+        await e.edit("💘💞💕💗")
+        await e.edit("SAYANG KAMU 💝💖💘")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💘💞💕💗")
+        await e.edit("SAYANG")
+        await e.edit("KAMU")
+        await e.edit("SELAMANYA 💕")
+        await e.edit("💘💘💘💘")
+        await e.edit("SAYANG")
+        await e.edit("KAMU")
+        await e.edit("SAYANG")
+        await e.edit("KAMU")
+        await e.edit("AKU CINTA KAMU")
+        await e.edit("MY HONEY SWEETY")
+        await e.edit("💕💞💘💝")
+        await e.edit("💘💕💞💝")
+        await e.edit("SAYANG KAMU MWAH💞")
+
+
+@register(outgoing=True, pattern="^.dino(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`DIN DINNN.....`")
+    sleep(1)
+    await typew.edit("`DINOOOOSAURUSSSSS!!`")
+    sleep(1)
+    await typew.edit("`🏃                        🦖`")
+    await typew.edit("`🏃                       🦖`")
+    await typew.edit("`🏃                      🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃   `LARII`          🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃WOARGH!   🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃  Huh-Huh           🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃          🦖`")
+    await typew.edit("`🏃         🦖`")
+    await typew.edit("`DIA SEMAKIN MENDEKAT!!!`")
+    sleep(1)
+    await typew.edit("`🏃       🦖`")
+    await typew.edit("`🏃      🦖`")
+    await typew.edit("`🏃     🦖`")
+    await typew.edit("`🏃    🦖`")
+    await typew.edit("`UDAHLAH CAPE, MATI AJA`")
+    sleep(1)
+    await typew.edit("`🧎🦖`")
+    sleep(2)
+    await typew.edit("`-TAMAT-`")
+
+
+@register(outgoing=True, pattern="^.gabut$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("`PERNAAHHHHH KAHHH KAUUU MENGIRA`")
+        await e.edit("`SEPEEERTIIIII APAAAA BENTUKKKKKKK CINTAAAA`")
+        await e.edit("`RAMBUUUT WARNAAA WARNII`")
+        await e.edit("`BAGAI GULALI`")
+        await e.edit("`IMUUUTTTTT LUCUUU`")
+        await e.edit("`WALAAUUUU TAK TERLALU TINGGI`")
+        await e.edit("`GW GABUUTTTT`")
+        await e.edit("`EMMMM BACOTNYA`")
+        await e.edit("`GABUTTTT WOI GABUT`")
+        await e.edit("🙈🙈🙈🙈")
+        await e.edit("🙉🙉🙉🙉")
+        await e.edit("🙈🙈🙈🙈")
+        await e.edit("🙉🙉🙉🙉")
+        await e.edit("`CILUUUKKK BAAAAA`")
+        await e.edit("🙉🙉🙉🙉")
+        await e.edit("🐢                       🚶")
+        await e.edit("🐢                      🚶")
+        await e.edit("🐢                     🚶")
+        await e.edit("🐢                    🚶")
+        await e.edit("🐢                   🚶")
+        await e.edit("🐢                  🚶")
+        await e.edit("🐢                 🚶")
+        await e.edit("🐢                🚶")
+        await e.edit("🐢               🚶")
+        await e.edit("🐢              🚶")
+        await e.edit("🐢             🚶")
+        await e.edit("🐢            🚶")
+        await e.edit("🐢           🚶")
+        await e.edit("🐢          🚶")
+        await e.edit("🐢         🚶")
+        await e.edit("🐢        🚶")
+        await e.edit("🐢       🚶")
+        await e.edit("🐢      🚶")
+        await e.edit("🐢     🚶")
+        await e.edit("🐢    🚶")
+        await e.edit("🐢   🚶")
+        await e.edit("🐢  🚶")
+        await e.edit("🐢 🚶")
+        await e.edit("🐢🚶")
+        await e.edit("🚶🐢")
+        await e.edit("🚶 🐢")
+        await e.edit("🚶  🐢")
+        await e.edit("🚶   🐢")
+        await e.edit("🚶    🐢")
+        await e.edit("🚶     🐢")
+        await e.edit("🚶      🐢")
+        await e.edit("🚶       🐢")
+        await e.edit("🚶        🐢")
+        await e.edit("🚶         🐢")
+        await e.edit("🚶          🐢")
+        await e.edit("🚶           🐢")
+        await e.edit("🚶            🐢")
+        await e.edit("🚶             🐢")
+        await e.edit("🚶              🐢")
+        await e.edit("🚶               🐢")
+        await e.edit("🚶                🐢")
+        await e.edit("🚶                 🐢")
+        await e.edit("🚶                  🐢")
+        await e.edit("🚶                   🐢")
+        await e.edit("🚶                    🐢")
+        await e.edit("🚶                     🐢")
+        await e.edit("🚶                      🐢")
+        await e.edit("🚶                       🐢")
+        await e.edit("🚶                        🐢")
+        await e.edit("🚶                         🐢")
+        await e.edit("🚶                          🐢")
+        await e.edit("🚶                           🐢")
+        await e.edit("🚶                            🐢")
+        await e.edit("🚶                             🐢")
+        await e.edit("🚶                              🐢")
+        await e.edit("🚶                               🐢")
+        await e.edit("🚶                                🐢")
+        await e.edit("🚶                                 🐢")
+        await e.edit("`AHHH MANTAP`")
+        await e.edit("🙉")
+        await e.edit("🙈")
+        await e.edit("🙉")
+        await e.edit("🙈")
+        await e.edit("🙉")
+        await e.edit("😂")
+        await e.edit("🐢                       🚶")
+        await e.edit("🐢                      🚶")
+        await e.edit("🐢                     🚶")
+        await e.edit("🐢                    🚶")
+        await e.edit("🐢                   🚶")
+        await e.edit("🐢                  🚶")
+        await e.edit("🐢                 🚶")
+        await e.edit("🐢                🚶")
+        await e.edit("🐢               🚶")
+        await e.edit("🐢              🚶")
+        await e.edit("🐢             🚶")
+        await e.edit("🐢            🚶")
+        await e.edit("🐢           🚶")
+        await e.edit("🐢          🚶")
+        await e.edit("🐢         🚶")
+        await e.edit("🐢        🚶")
+        await e.edit("🐢       🚶")
+        await e.edit("🐢      🚶")
+        await e.edit("🐢     🚶")
+        await e.edit("🐢    🚶")
+        await e.edit("🐢   🚶")
+        await e.edit("🐢  🚶")
+        await e.edit("🐢 🚶")
+        await e.edit("🐢🚶")
+        await e.edit("🚶🐢")
+        await e.edit("🚶 🐢")
+        await e.edit("🚶  🐢")
+        await e.edit("🚶   🐢")
+        await e.edit("🚶    🐢")
+        await e.edit("🚶     🐢")
+        await e.edit("🚶      🐢")
+        await e.edit("🚶       🐢")
+        await e.edit("🚶        🐢")
+        await e.edit("🚶         🐢")
+        await e.edit("🚶          🐢")
+        await e.edit("🚶           🐢")
+        await e.edit("🚶            🐢")
+        await e.edit("🚶             🐢")
+        await e.edit("🚶              🐢")
+        await e.edit("🚶               🐢")
+        await e.edit("🚶                🐢")
+        await e.edit("🚶                 🐢")
+        await e.edit("🚶                  🐢")
+        await e.edit("🚶                   🐢")
+        await e.edit("🚶                    🐢")
+        await e.edit("🚶                     🐢")
+        await e.edit("🚶                      🐢")
+        await e.edit("🚶                       🐢")
+        await e.edit("🚶                        🐢")
+        await e.edit("🚶                         🐢")
+        await e.edit("🚶                          🐢")
+        await e.edit("🚶                           🐢")
+        await e.edit("🚶                            🐢")
+        await e.edit("🚶                             🐢")
+        await e.edit("🚶                              🐢")
+        await e.edit("🚶                               🐢")
+        await e.edit("🚶                                🐢")
+        await e.edit("`GABUT`")
+
+
+@register(outgoing=True, pattern="^.terkadang(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Terkadang`")
+    sleep(1)
+    await typew.edit("`Mencintai Seseorang`")
+    sleep(1)
+    await typew.edit("`Hanya Akan Membuang Waktumu`")
+    sleep(1)
+    await typew.edit("`Ketika Waktumu Habis`")
+    sleep(1)
+    await typew.edit("`Tambah Aja 5000`")
+    sleep(1)
+    await typew.edit("`Bercanda`")
+
+
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern="^.mf$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("`maaf ga dulu` **ミ(ノ;_ _)ノ=3** ")
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 11)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "cinta":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Connecting Ke Server Cinta`",
+            "`Mencari Target Cinta`",
+            "`Mengirim Cintaku.. 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 84%\n█████████████████████▒▒▒▒ `",
+            "`Mengirim Cintaku.. 100%\n█████████CINTAKU███████████ `",
+            f"`Cintaku Sekarang Sepenuhnya Terkirim Padamu, Dan Sekarang Aku Sangat Mencintai Mu, I Love You 💞`",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 11])
+
+
+@register(outgoing=True, pattern="^.gombal(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Hai, AKU CINTA KAMU 💞`")
+    sleep(1)
+    await typew.edit("`AKU CINTA KAMU BANGET!`")
+    sleep(1)
+    await typew.edit("`AKU BUTUH KAMU BUAT LENGKAPIN HIDUPKU!`")
+    sleep(1)
+    await typew.edit("`HIDUPKU TANPA KAMU HAMPA TAK BERARTI!`")
+    sleep(1)
+    await typew.edit("`JADILAH BELAHAN JIWAKU💕💗`")
+    sleep(1)
+    await typew.edit("`JADILAH SUMBER BAHAGIAKU💗💞`")
+    sleep(1)
+    await typew.edit("`JADILAH SUMBER SENYUMKU💝💗`")
+    sleep(1)
+    await typew.edit("`AKU SAYANG KAMU BANGET💟💖`")
+    sleep(1)
+    await typew.edit("`AKU CINTA KAMU BANGET💘💓`")
+    sleep(1)
+    await typew.edit("`TERIMALAH CINTA DARIKU MWAHH😘💕💞`")
+
+
+# Create by myself @localheart
+
+CMD_HELP.update(
+    {
+        "animasi3": "`.gabut` ; `.dino`\
+    \nUsage: ntahlah gabut doang.\
+    \n\n`.gombal`\
+    \nUsage: buat bercanda\
+    \n\n`.cinta`\
+    \nUsage: mengirim cintamu ke seseorang.\
+    \n\n`.sayang`\
+    \nUsage: untuk jadi buaya.\
+    \n\n`.terkadang`\
+    \nUsage: Auk dah iseng doang."
+    }
+)
