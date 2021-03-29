1fc63f5fa289232b30f45bfe6d19a09ce7a9c713
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-03-29 14:28:47 +0800

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -17,7 +17,7 @@ async def ultiny(event):
         return
     xx = await event.edit("`Bimsalabim Jadi Kecil...`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("resources/extras/spacebot.png")
+    im1 = Image.open("kampangbot.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
