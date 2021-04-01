937735a49f0462fcd50655bef0c009bd453bfa17
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-04-01 19:48:27 +0800

    Update animasi3.py

--- userbot/modules/animasi3.py
+++ userbot/modules/animasi3.py
@@ -345,16 +345,17 @@ async def typewriter(typew):
 # Create By Alvin Lord-Userbot
 
 CMD_HELP.update({
-    "animasi3": "`.gabut` ; `.dino`\
-    \nUsage: ntahlah gabut doang.\
-    \n\n`.gombal`\
-    \nUsage: buat bercanda\
-    \n\n`.cinta`\
-    \nUsage: mengirim cintamu ke seseorang.\
-    \n\n`.sayang`\
-    \nUsage: untuk jadi buaya.\
-    \n\n`.terkadang`\
-    \nUsage: Pas Edit Gi Bdmd Jadi Gini.\
-    \n\n`.fck`\
-    \nUsage: Fuck "
+    "animasi3": 
+    "`.gabut` ; `.dino`\
+\nUsage: ntahlah gabut doang.\
+\n\n`.gombal`\
+\nUsage: buat bercanda\
+\n\n`.cinta`\
+\nUsage: mengirim cintamu ke seseorang.\
+\n\n`.sayang`\
+\nUsage: untuk jadi buaya.\
+\n\n`.terkadang`\
+\nUsage: Pas Edit Gi Bdmd Jadi Gini.\
+\n\n`.fck`\
+\nUsage: Fuck "
 })
