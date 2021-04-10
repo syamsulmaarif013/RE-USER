8ef6408670afa45398fdcc02002603b9d64acd9a
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-04-10 08:28:26 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"BOT-ANGKASA 🌐 V{BOT_VER} [ALHAMDULILLAH MENYALA 🔥]")
+    f"SPACE-BOT 🌐 V{BOT_VER} [DIAKTIFKAN ANJENGGG 🔥]")
 
 
 if len(argv) not in (1, 3, 4):
